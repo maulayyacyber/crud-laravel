3b85c2027be63dfed9d2e081ed46be1a78db6c83
maulayyacyber <ridaulmaulayya@gmail.com>
2016-08-30 00:28:28 +0700

    setting up database

---
+++ database/migrations/2016_08_29_164848_create_tbl_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTblBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('books', function($table){
+         $table->increments('id');
+         $table->string('title')->unique();
+         $table->string('author');
+         $table->string('description');
+         $table->string('price');
+         $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
